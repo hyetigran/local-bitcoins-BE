f3128d6eb32440b073f7aa30aab9a0423fc15c96
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-03-19 18:46:48 -0400

    add restricted middleware

---
+++ knexfile.js
@@ -0,0 +1,43 @@
+// Update with your config settings.
+require("dotenv").config(); // load .env variables
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./dev.sqlite3"
+    }
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+};
