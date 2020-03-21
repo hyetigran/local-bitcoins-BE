d5397fb15b0f87d19b1da2935de2a9f43ab5bdfe
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-03-21 01:28:09 -0400

    create orders table and ran migration success

---
+++ data/migrations/20200321012509_create_orders_table.js
@@ -0,0 +1,39 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("orders", orders => {
+    orders.increments();
+    orders
+      .boolean("complete")
+      .notNullable()
+      .defaultTo(false);
+    orders
+      .boolean("cancelled")
+      .notNullable()
+      .defaultTo(false);
+    orders
+      .integer("maker_id")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    orders
+      .integer("taker_id")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    orders
+      .integer("offer_id")
+      .unsigned()
+      .references("id")
+      .inTable("offers")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    orders.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("orders");
+};
