0b451e4ccbdfde9f1ebb40d2b544f734c4b90d96
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-03-21 01:24:21 -0400

    create offers table and ran migration success

---
+++ data/migrations/20200321002716_create_offers_table.js
@@ -0,0 +1,38 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("offers", offers => {
+    offers.increments();
+    offers.boolean("buyBCH").notNullable();
+    offers.string("country").notNullable();
+    offers.string("city").notNullable();
+    offers.string("payment_method").notNullable();
+    offers.string("currency_type").notNullable();
+    offers
+      .boolean("dynamic_pricing")
+      .notNullable()
+      .defaultTo(true);
+
+    offers.integer("margin").notNullable();
+    offers.boolean("margin_above").notNullable();
+    offers.string("market_exchange").notNullable();
+    offers.integer("limit_min").defaultTo(0);
+    offers.integer("limit_max");
+    offers.string("headline");
+    offers.string("trade_terms", 1000);
+    offers.time("open_hours");
+    offers.time("close_hours");
+    offers.boolean("verified_only").notNullable();
+    offers.boolean("pause").defaultTo(false);
+    offers
+      .integer("maker_id")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    offers.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("offers");
+};
