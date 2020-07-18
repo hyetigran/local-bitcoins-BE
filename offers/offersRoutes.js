898010eae0bf10315e0652f66beb47675cc78a19
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-07-18 00:24:18 -0400

    per original, resource does not require login to view

--- offers/offersRoutes.js
+++ offers/offersRoutes.js
@@ -12,7 +12,7 @@ router.post(
 );
 
 router.get("/:id([0-9]+)", checkLoggedIn, offersController.getMyOffers);
-router.get("/offer/:id([0-9]+)", checkLoggedIn, offersController.getOffer);
+router.get("/offer/:id([0-9]+)", offersController.getOffer);
 
 router.put(
   "/:userId([0-9]+)/:offerId([0-9]+)",
