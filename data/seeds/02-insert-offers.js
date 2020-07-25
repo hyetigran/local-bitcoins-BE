79a0340457cae22d43c4f0fd1832ed68fe12ebc1
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-07-25 19:25:59 -0400

    create fake offers seed for dev dv

---
+++ data/seeds/02-insert-offers.js
@@ -0,0 +1,28 @@
+const faker = require("faker");
+const createFakeOffers = (value) => {
+  return {
+    buyBCH: faker.random.boolean(),
+    country: faker.address.country(),
+    city: faker.address.city(),
+    payment_method: "Cash in person",
+    currency_type: faker.finance.currencyCode(),
+    currency_symbol: faker.finance.currencySymbol(),
+    dynamic_pricing: true,
+    margin: parseInt(Math.random() * 10) + 1,
+    margin_above: faker.random.boolean(),
+    verified_only: faker.random.boolean(),
+    maker_id: value,
+  };
+};
+exports.seed = function (knex) {
+  let fakeOffers = [];
+  for (let i = 1; i < 21; i++) {
+    fakeOffers.push(createFakeOffers(i));
+  }
+  return knex("offers")
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex("offers").insert(fakeOffers);
+    });
+};
