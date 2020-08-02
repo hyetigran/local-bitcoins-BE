1f9423a918d4695b3b8dc777e61d0d2689f45a7f
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-08-02 13:06:31 -0400

    updated seed file and ran migrations

--- data/seeds/03-insert-orders.js
+++ data/seeds/03-insert-orders.js
@@ -9,7 +9,7 @@ const createFakeOrders = (value) => {
     price_bch: "250",
     bch_amount: "2",
     fiat_amount: "500",
-    is_buying: boolVal,
+    is_maker_buying: boolVal,
   };
 };
 exports.seed = function (knex) {
